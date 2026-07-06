a2a5307a7b2c365a6b8fdb19cd610749dee15919
YouanesAttia <youanesatia6@gmail.com>
2026-07-06 15:34:09 +0300

    Day 31: Python data model — FrenchDeck and Vector2D dunder methods

--- week5_python_core/day1/day31_data_model.py
+++ week5_python_core/day1/day31_data_model.py
@@ -1,5 +1,6 @@
 import collections
 from random import choice
+from math import hypot
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
@@ -24,3 +25,25 @@ choice(deck)
 
 for card in deck:
     print(card)
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Vector(%r, %r)' % (self.x, self.y)
+
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y 
+        return Vector(x, y)
+
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
