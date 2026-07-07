d1906fd4b7535fc5646f88870e1aa79bde2af366
YouanesAttia <youanesatia6@gmail.com>
2026-07-07 22:23:54 +0300

    Rewrite 5 loops as list comprehensions (filter evens, map to uppercase, 10×10 table, long words, flatten nested list)

---
+++ week5_python_core/day2/day32_sequences.py
@@ -0,0 +1,10 @@
+nums = [n for n in range(10)]
+words = ["apple", "banana", "cherry", "cat", "elephant", "dog", "giraffe", "lion"]
+matrix = [[1, 2], [3, 4], [5, 6]]
+
+even = [n for n in nums if n % 2 == 0]
+table = [[n * m for n in nums] for m in nums]
+upper = [word.upper() for word in words]
+long = [word for word in words if len(word) > 5]
+flattened = [num for row in matrix for num in row]
+
