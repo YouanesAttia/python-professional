25402bdd5c491dd19da4d887d32ea6c7e12b1b5f
YouanesAttia <youanesatia6@gmail.com>
2026-07-07 14:51:52 +0300

    Slicing: learning phase

--- week5_python_core/day2/listcomp.py
+++ week5_python_core/day2/listcomp.py
@@ -51,3 +51,40 @@ for name, cc, pop, (latitude, longitude) in metro_areas:  # 2
 City = namedtuple('City', 'name country population coordinates')
 tokyo = City('Tokyo', 'JP', 36.933, (35.689722, 139.691667))
 tokyo.population  # 36.933
+
+# Slicing
+l = [10, 20, 30, 40, 50, 60]
+l[:2] # [10, 20]
+l[2:] # [30, 40, 50, 60]
+
+# sequence[start : stop : step]
+s = 'bicycle'
+s[::3] # 'bye'
+s[::-1] # 'elcycib'
+# These two are doing the exact same thing:
+part = s[0:5:2]
+part = s[slice(0, 5, 2)]
+
+invoice = """
+0.....6..................................40........52..55........
+1909  Pimoroni PiBrella                    $17.50   3    $52.50
+1489  6mm Tactile Switch x20                $4.95   2     $9.90
+1510  Panavise Jr. - PV-201                $28.00   1    $28.00
+1601  PiTFT Mini Kit 320x240               $34.95   1    $34.95
+"""
+SKU = slice(0, 6)
+DESCRIPTION = slice(6, 40)
+UNIT_PRICE = slice(40, 52)
+QUANTITY = slice(52, 55)
+ITEM_TOTAL = slice(55, None)
+line_items = invoice.split('\n')[2:]
+for item in line_items:
+    print(item[UNIT_PRICE], item[DESCRIPTION])
+
+
+# Assigning to Slices
+l = list(range(10)) # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+l[2:5] = [20, 30]   # [0, 1, 20, 30, 5, 6, 7, 8, 9]
+del l[5:7]          # [0, 1, 20, 30, 5, 8, 9]
+l[2:5] = 100        # TypeError: can only assign an iterable
+
