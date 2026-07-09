96f8dcdbb592f221814be699d7f1ff42c348dc94
YouanesAttia <youanesatia6@gmail.com>
2026-07-09 23:45:08 +0300

    Decorators & Variable scopes

--- week5_python_core/day4/decorators.py
+++ week5_python_core/day4/decorators.py
@@ -1,3 +1,6 @@
+import time
+import functools
+
 def decorate(func):
     def wrapper():
         print("Before")
@@ -112,3 +115,82 @@ def f3(a):           # This will run
 
 
 # Closures
+#### a closure is a function with an extended scope that encompasses nonglobal variables referenced in the body of the function but not defined there.
+class Averager():
+    def __init__(self):
+        self.series = []
+    
+    def __call__(self, new_value):
+        self.series.append(new_value)
+        total = sum(self.series)
+        return total/len(self.series)
+
+class Averager1():
+    def __init__(self):
+        self.total = 0.0
+        self.count = 0
+    
+    def __call__(self, new_value):
+        self.count += 1
+        self.total += new_value
+        return self.total / self.count
+
+def make_averager():
+    series = []
+    def averager(new_value):
+        series.append(new_value)
+        total = sum(series)
+        return total/len(series)
+    return averager
+
+avg = make_averager()
+avg[10]
+avg[11]
+avg[12]
+
+avg.__code__.co_varnames          # ('new_value', 'total')
+avg.__code__.co_freevars          # ('series',)
+avg.__closure__[0].cell_contents  # [10, 11, 12]
+
+def make_averager():
+    count = 0
+    total = 0
+    def averager(new_val):
+        # count += 1    # UnboundLocalError: local variable 'count' referenced before assignment
+        # total += new_val
+        nonlocal count, total
+        count += 1
+        total += new_val
+        return total/count
+    return averager
+
+# Implementing a Simple Decorator
+def clock(func):
+    def clocked(*args):
+        t0 = time.perf_counter()
+        result = func(*args)
+        elapsed = time.perf_counter() - t0
+        name = func.__name__
+        arg_str = ', '.join(repr(arg) for arg in args)
+        print('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
+        return result
+    return clocked
+
+def clock2(func):
+    @functools.wraps(func)
+    def clocked(*args, **kwargs):
+        t0 = time.time()
+        result = func(*args, **kwargs)
+        elapsed = time.time() - t0
+        name = func.__name__
+        arg_lst = []
+        if args:
+            arg_lst.append(', '.join(repr(arg) for arg in args))
+            
+        if kwargs:
+            pairs = ['%s=%r' % (k, w) for k, w in sorted(kwargs.items())]
+            arg_lst.append(', '.join(pairs))
+        arg_str = ', '.join(arg_lst)        
+        print('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
+        return result
+    return clocked
