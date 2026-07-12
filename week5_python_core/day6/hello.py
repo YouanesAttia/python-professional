7f5fb95ac93ab408384e88473e7b3d2534997865
YouanesAttia <youanesatia6@gmail.com>
2026-07-12 14:31:02 +0300

    Flask introduction

---
+++ week5_python_core/day6/hello.py
@@ -0,0 +1,72 @@
+from flask import Flask 
+from markupsafe import escape
+from flask import url_for
+from flask import request
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route("/hello")
+def hello():
+    return 'Hello, World'
+
+
+# Variable Rules
+@app.route('/usr/<username>')
+def show_user_profile(username):
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    return f'Subpath {escape(subpath)}'
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/about')
+def about():
+    return 'The about page'
+
+
+# URL Building
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
+
+
+# HTTP Methods: They are equivelant
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+
+@app.get('/login')
+def login_get():
+    return show_the_login_form()
+
+@app.post('/login')
+def login_post():
+    return do_the_login()
+
+
+# url_for('static', filename='style.css')
+# The file has to be stored on the filesystem as static/style.css.
