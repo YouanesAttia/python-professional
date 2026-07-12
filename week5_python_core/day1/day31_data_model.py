946569c55cc08f65869f1426be66f0cd975c251d
YouanesAttia <youanesatia6@gmail.com>
2026-07-12 15:52:57 +0300

    Type hints + pytest

--- week5_python_core/day1/day31_data_model.py
+++ week5_python_core/day1/day31_data_model.py
@@ -1,49 +1,56 @@
-import collections
-from random import choice
-from math import hypot
+import math
+from typing import List, NamedTuple, Union, Any, Self
 
-Card = collections.namedtuple('Card', ['rank', 'suit'])
+class Card(NamedTuple):
+    rank: str
+    suit: str
 
 class FrenchDeck:
-    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
-    suits = 'spades diamonds clubs hearts'.split()
+    ranks: List[str] = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits: List[str] = 'spades diamonds clubs hearts'.split()
 
-    def __init__(self):
-        self._cards = [Card(rank, suit) for rank in self.ranks
-                                        for suit in self.suits]
+    def __init__(self) -> None:
+        self._cards: List[Card] = [
+            Card(rank, suit) 
+            for rank in self.ranks 
+            for suit in self.suits
+        ]
     
-    def __len__(self):
-        return len(self._card)
+    def __len__(self) -> int:
+        return len(self._cards)
     
-    def __getitem__(self, position):
-        return self._cards[position]    
+    def __getitem__(self, position: Union[int, slice]) -> Union[Card, List[Card]]:
+        return self._cards[position]
 
-deck = FrenchDeck()
-len(deck)   # 52
-deck[0]     # Card(rank='2', suit='spades')
-choice(deck)
+class Vector2D:
+    x: float
+    y: float
 
-for card in deck:
-    print(card)
-
-class Vector:
-    def __init__(self, x, y):
+    def __init__(self, x: float, y: float) -> None:
         self.x = x
         self.y = y
 
-    def __repr__(self):
-        return 'Vector(%r, %r)' % (self.x, self.y)
+    def __repr__(self) -> str: 
+        return f'Vector2D({self.x!r}, {self.y!r})'
 
-    def __abs__(self):
-        return hypot(self.x, self.y)
+    def __abs__(self) -> float:
+        return math.hypot(self.x, self.y)
 
-    def __bool__(self):
+    def __bool__(self) -> bool:
         return bool(abs(self))
 
-    def __add__(self, other):
-        x = self.x + other.x
-        y = self.y + other.y 
-        return Vector(x, y)
+    def __add__(self, other: Any) -> Self:
+        if not isinstance(other, Vector2D):
+            return NotImplemented
+        return self.__class__(self.x + other.x, self.y + other.y)
 
-    def __mul__(self, scalar):
-        return Vector(self.x * scalar, self.y * scalar)
+    def __mul__(self, scalar: float) -> Self:
+        return self.__class__(self.x * scalar, self.y * scalar)
+
+deck = FrenchDeck()
+card_count: int = len(deck)
+first_card: Union[Card, List[Card]] = deck[0]
+
+v1 = Vector2D(2, 4)
+v2 = Vector2D(2, 1)
+v3 = v1 + v2 
