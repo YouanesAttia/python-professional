0d742b80202d46541197bee99a9edcb261eb2bf7
YouanesAttia <youanesatia6@gmail.com>
2026-07-14 18:27:09 +0300

    Array creation

---
+++ week6_data_science/day1/NumPy_web.py
@@ -0,0 +1,153 @@
+import numpy as np
+from numpy.linalg import inv, qr
+import matplotlib.pyplot as plt
+
+my_arr = np.arange(1000000)
+my_list = list(range(1000000))
+# If we tried to multiply each element by any num, you will notice that numpy is much faster
+
+
+# 4.1 The NumPy ndarray: A Multidimensional Array Object
+data = np.random.randn(2, 3)
+print (data)
+print (data * 10)
+print (data +data)
+print (data.shape)             # (2,3)
+print (data.dtype)             # float64
+
+## Creating ndarrays
+data1 = [6, 7.5, 8, 0, 1]
+arr1 = np.array(data1)
+print (arr1)
+
+data2 = [[1, 2, 3, 4], [5, 6, 7, 8]]
+arr2 = np.array(data2)
+print (arr2.shape)
+
+arr3 = np.zeros(10)
+arr4 = np.ones(10)
+arr5 = np.empty(10)
+
+## Data Types for ndarrays
+arr1 = np.array([1, 2, 3], dtype=np.float64)
+arr2 = np.array([1, 2, 3], dtype=np.int64)
+print (arr2.astype(np.bool))
+
+## Arithmetic with NumPy Arrays
+#### Arrays are important because they enable you to express batch operations on
+#### data without writing any for loops. NumPy users call this vectorization. 
+arr1 = np.arange(10, dtype=np.float64)
+print(arr1 * arr1)
+print(arr1 - arr1)
+print(1/arr1)
+arr2 = np.arange(5, 15)
+print (arr2 > arr1)
+
+## Basic Indexing and Slicing
+arr1[5:8] = 12         # 
+arr_slice = arr1[5:8]  # Only view of the data
+arr_slice[1] = 12345   # arr1 = [ 0,  1,  2,  3,  4, 12, 12345, 12,  8,  9]
+arr1[:] = 12           # arr1 = [12, 12, 12, 12, 12, 12, 12, 12, 12, 12]
+
+
+## Boolean Indexing
+names = np.array(['Bob', 'Joe', 'Will', 'Bob', 'Will', 'Joe', 'Joe'])
+data = np.random.randn(7, 4)
+print(data[names == 'Bob', 2:])       # It will print row 0 and row 3
+
+
+## Fancy Indexing
+arr = np.empty((8, 4))
+for i in range(8):
+    arr[i] = i
+arr[[4, 3, 0, 6]]
+"""
+array([[ 4.,  4.,  4.,  4.],
+       [ 3.,  3.,  3.,  3.],
+       [ 0.,  0.,  0.,  0.],
+       [ 6.,  6.,  6.,  6.]])
+"""
+arr = np.arange(32).reshape((8, 4))
+arr[[1, 5, 7, 2], [0, 3, 1, 2]]        # array([ 4, 23, 29, 10])
+
+
+## Transposing Arrays and Swapping Axes
+arr = np.arange(15).reshape((3, 5))
+print (arr)
+print (arr.T)
+np.dot(arr.T, arr)
+
+
+# 4.2 Universal Functions: Fast Element-Wise Array Functions
+arr = np.random.randn(7) * 5
+remainder, whole_part = np.modf(arr)
+
+
+# 4.3 Array-Oriented Programming with Arrays
+points = np.arange(-5, 5, 0.01) # 1000 equally spaced points
+xs, ys = np.meshgrid(points, points)
+z = np.sqrt(xs**2 + ys**2)
+plt.imshow(z, cmap=plt.cm.gray); plt.colorbar()
+plt.title("Image")
+plt.show()
+
+## Expressing Conditional Logic as Array Operations
+xarr = np.array([1.1, 1.2, 1.3, 1.4, 1.5])
+yarr = np.array([2.1, 2.2, 2.3, 2.4, 2.5])
+cond = np.array([True, False, True, True, False])
+res = [(x if c else y) for x, y, c in zip(xarr, yarr, cond)]
+res = np.where(cond, xarr, yarr)
+
+## Mathematical and Statistical Methods
+arr = np.random.randn(5, 4)
+arr.mean()
+arr.sum()
+arr.mean(axis=1)      # compute mean across the columns
+arr.sum(axis=0)       # compute sum down the rows.
+
+## Methods for Boolean Arrays
+arr = np.random.randn(100)
+print ((arr>0).sum()) # Number of positive values
+
+bools = np.array([False, False, True, False])
+bools.any()           # True
+bools.all()           # False
+
+## Sorting
+arr.sort()
+arr = np.random.randn(5, 3)
+arr.sort(1)     # Will sort the 2 row
+###### returns a sorted copy of an array instead of modifying the array in-place. 
+
+## Unique and Other Set Logic
+names = np.array(['Bob', 'Joe', 'Will', 'Bob', 'Will', 'Joe', 'Joe'])
+np.unique(names)      # ['Bob', 'Joe', 'Will']
+values = np.array([6, 0, 0, 3, 2, 5, 6])
+
+
+# 4.4 File Input and Output with Arrays
+arr = np.arange(10)
+np.save('some_array', arr)
+arr = np.load('some_array.npy')
+print (arr)
+
+np.savez('array_archive.npz', a=arr, b=arr)
+arch = np.load('array_archive.npz')    # Dict
+arch['a']                              # [0 1 2 3 4 5 6 7 8 9]
+np.savez_compressed('arrays_compressed.npz', a=arr, b=arr)
+
+
+# 4.5 Linear Algebra
+x = np.array([[1., 2., 3.], [4., 5., 6.]])
+y = np.array([[6., 23.], [-1, 7], [8, 9]])
+z = x.dot(y)
+z = np.dot(x, y)
+z = x @ y
+"""
+[[  28.,   64.],
+ [  67.,  181.]]
+"""
+
+X = np.random.randn(5, 5)
+mat = X.T.dot(X)
+print(mat.dot(inv(mat)))
