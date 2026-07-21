cda539c3bc8d232080a2d34680950c5cc0d12a2a
YouanesAttia <youanesatia6@gmail.com>
2026-07-21 17:00:35 +0300

    JSON Data

--- week6_data_science/day2/data_manip.py
+++ week6_data_science/day2/data_manip.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import sys
 import csv
+import json
 
 # 6.1 Reading and Writing Data in Text Format
 df = pd.read_csv('./example/ex1.csv')
@@ -42,3 +43,35 @@ for line in reader:
 with open('examples/ex7.csv') as f:
     lines = list(csv.reader(f))
     header, values = lines[0], lines[1:]
+    data_dict = {h: v for h, v in zip(header, zip(*values))}
+
+
+## JSON Data
+######  All of the keys in an object must be strings
+obj = """
+{"name": "Wes",
+"places_lived": ["United States", "Spain", "Germany"],
+"pet": null,
+"siblings": [{"name": "Scott", "age": 30, "pets": ["Zeus", "Zuko"]},
+            {"name": "Katie", "age": 38,
+            "pets": ["Sixes", "Stache", "Cisco"]}]
+}
+"""
+
+result = json.loads(obj)       # From JSON to Python
+asjson = json.dumps(result)    # From Python to JSON
+siblings = pd.DataFrame(result['siblings'], columns=['name', 'age'])
+"""
+    name  age
+0  Scott   30
+1  Katie   38
+"""
+data = pd.read_json('example/example.json')
+"""
+   a  b  c
+0  1  2  3
+1  4  5  6
+2  7  8  9
+"""
+print(data.to_json())      # {"a":{"0":1,"1":4,"2":7},"b":{"0":2,"1":5,"2":8},"c":{"0":3,"1":6,"2":9}}
+print(data.to_json(orient='records'))  # [{"a":1,"b":2,"c":3},{"a":4,"b":5,"c":6},{"a":7,"b":8,"c":9}]
