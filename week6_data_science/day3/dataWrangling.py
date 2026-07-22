13833c5d0892cafaad9e46a5961c1ff9c576b2a7
YouanesAttia <youanesatia6@gmail.com>
2026-07-22 17:43:01 +0300

    Combining and Merging Datasets

--- week6_data_science/day3/dataWrangling.py
+++ week6_data_science/day3/dataWrangling.py
@@ -347,3 +347,40 @@ two    0.0  1.0  NaN  NaN
 three  NaN  NaN  5.0  6.0
 """
 
+## Combining Data with Overlap
+a = pd.Series([np.nan, 2.5, np.nan, 3.5, 4.5, np.nan],
+              index=['f', 'e', 'd', 'c', 'b', 'a'])
+
+b = pd.Series(np.arange(len(a), dtype=np.float64),
+              index=['f', 'e', 'd', 'c', 'b', 'a'])
+
+b[-1] = np.nan
+np.where(pd.isnull(a), b, a)         # array([ 0. ,  2.5,  2. ,  3.5,  4.5,  nan])
+
+b[:-2].combine_first(a[2:])
+"""
+a    NaN
+b    4.5
+c    3.0
+d    2.0
+e    1.0
+f    0.0
+"""
+
+df1 = pd.DataFrame({'a': [1., np.nan, 5., np.nan],
+                    'b': [np.nan, 2., np.nan, 6.],
+                    'c': range(2, 18, 4)})
+
+df2 = pd.DataFrame({'a': [5., 4., np.nan, 3., 7.],
+                    'b': [np.nan, 3., 4., 6., 8.]})
+
+df1.combine_first(df2)
+"""
+     a    b     c
+0  1.0  NaN   2.0
+1  4.0  2.0   6.0
+2  5.0  4.0  10.0
+3  3.0  6.0  14.0
+4  7.0  8.0   NaN
+"""
+# combine_first = "Take everything from the left. Fill its missing values from the right. If the right has extra index labels, append them too."
