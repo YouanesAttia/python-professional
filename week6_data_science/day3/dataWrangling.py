f5a5b15914d51dc08fd7cb29ee06144b389eec9f
YouanesAttia <youanesatia6@gmail.com>
2026-07-22 15:00:22 +0300

    Database-Style DataFrame Joins

--- week6_data_science/day3/dataWrangling.py
+++ week6_data_science/day3/dataWrangling.py
@@ -112,3 +112,109 @@ two 0  3  4
     3  6  1
 """
 frame2.reset_index()           # does the opposite of set_index
+
+
+# 8.2 Combining and Merging Datasets
+## Database-Style DataFrame Joins
+#### Many-to-One combination
+df1 = pd.DataFrame({'key': ['b', 'b', 'a', 'c', 'a', 'a', 'b'],
+                    'data1': range(7)})
+df2 = pd.DataFrame({'key': ['a', 'b', 'd'],
+                    'data2': range(3)})
+pd.merge(df1, df2, on='key')
+########  If that information is not specified, merge uses the overlapping column names as the keys
+"""
+   data1 key  data2
+0      0   b      1
+1      1   b      1
+2      6   b      1
+3      2   a      0
+4      4   a      0
+5      5   a      0
+"""
+
+df3 = pd.DataFrame({'lkey': ['b', 'b', 'a', 'c', 'a', 'a', 'b'],
+                    'data1': range(7)})
+df4 = pd.DataFrame({'rkey': ['a', 'b', 'd'],
+                    'data2': range(3)})
+
+pd.merge(df3, df4, left_on='lkey', right_on='rkey')    # Do inner Join; only couples allowed
+"""
+   data1 lkey  data2 rkey
+0      0    b      1    b
+1      1    b      1    b
+2      6    b      1    b
+3      2    a      0    a
+4      4    a      0    a
+5      5    a      0    a
+"""
+pd.merge(df1, df2, how='outer')           # Outer Join: takes the union of the keys
+"""
+   data1 key  data2
+0    0.0   b    1.0
+1    1.0   b    1.0
+2    6.0   b    1.0
+3    2.0   a    0.0
+4    4.0   a    0.0
+5    5.0   a    0.0
+6    3.0   c    NaN
+7    NaN   d    2.0
+"""
+pd.merge(df1, df2, how='left')           # Use all key combinations found in the left table
+
+#### Many-to-Many combination
+df1 = pd.DataFrame({'key': ['b', 'b', 'a', 'c', 'a', 'b'],
+                    'data1': range(6)})
+df2 = pd.DataFrame({'key': ['a', 'b', 'a', 'b', 'd'],
+                    'data2': range(5)})
+pd.merge(df1, df2, on='key', how='left')
+"""
+    data1 key  data2
+0       0   b    1.0
+1       0   b    3.0
+2       1   b    1.0
+3       1   b    3.0
+4       2   a    0.0
+5       2   a    2.0
+6       3   c    NaN
+7       4   a    0.0
+8       4   a    2.0
+9       5   b    1.0
+10      5   b    3.0
+"""
+
+pd.merge(df1, df2, how='inner')
+"""
+   data1 key  data2
+0      0   b      1
+1      0   b      3
+2      1   b      1
+3      1   b      3
+4      5   b      1
+5      5   b      3
+6      2   a      0
+7      2   a      2
+8      4   a      0
+9      4   a      2
+"""
+
+left = pd.DataFrame({'key1': ['foo', 'foo', 'bar'],
+                     'key2': ['one', 'two', 'one'],
+                     'lval': [1, 2, 3]})
+
+right = pd.DataFrame({'key1': ['foo', 'foo', 'bar', 'bar'],
+                      'key2': ['one', 'one', 'one', 'two'],
+                      'rval': [4, 5, 6, 7]})
+
+pd.merge(left, right, on=['key1', 'key2'], how='outer')
+
+"""
+  key1 key2  lval  rval
+0  foo  one   1.0   4.0
+1  foo  one   1.0   5.0
+2  foo  two   2.0   NaN
+3  bar  one   3.0   6.0
+4  bar  two   NaN   7.0
+"""
+
+pd.merge(left, right, on='key1', suffixes=('_left', '_right'))
